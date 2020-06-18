a09aefa8c718cd2eca4ef8e5a5e063b3958df8ca
paras <paraskanwar30@gmail.com>
2020-06-18 20:36:47 +0530

    added

--- src/Component/page2.jsx
+++ src/Component/page2.jsx
@@ -1,13 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Button from "./myButton/b";
+import axios from "axios";
 import a from "../App";
 export default (props) => {
+  const [quote, setQuote] = useState(null);
   const b = useContext(a);
   console.log(b);
   return (
     <div>
       <Button {...props}></Button>
-      <h1>{b}</h1>
+      <button
+        onClick={() => {
+          axios
+            .post("https://anonymity.uno/api/today/quote/101")
+            .then((res) => {
+              console.log(res);
+              setQuote(res.data);
+            })
+            .catch((e) => {
+              console.log(e.message);
+            });
+        }}
+      >
+        get quotes
+      </button>
+      {quote ? quote.text : "nothing"}
     </div>
   );
 };
